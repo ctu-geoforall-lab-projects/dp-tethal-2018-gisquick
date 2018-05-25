a4528eb1421808ae4596e342b0da0d748d1f8876
David_Tethal <davidtethal@gmail.com>
2018-05-25 19:38:25 +0200

    create vue build for deploy

--- clients/vue-js/config/prod.env.js
+++ clients/vue-js/config/prod.env.js
@@ -1,5 +1,6 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  SERVER_URL: '"https://projects.gisquick.org"'
+  // SERVER_URL: '"https://projects.gisquick.org"'
+  SERVER_URL: '"http://localhost:8000"'
 }
